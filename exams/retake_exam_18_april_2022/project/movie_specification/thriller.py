514496968831c692f936469fc548b4ae5951aade
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-30 19:37:29 +0100

    Create thriller.py

---
+++ exams/retake_exam_18_april_2022/project/movie_specification/thriller.py
@@ -0,0 +1,9 @@
+from exams.retake_exam_18_april_2022.project.movie_specification.movie import Movie
+from exams.retake_exam_18_april_2022.project.user import User
+
+
+class Thriller(Movie):
+    DEFAULT_AGE_RESTRICTION = 16
+
+    def __init__(self, title: str, year: int, owner: User, age_restriction=DEFAULT_AGE_RESTRICTION):
+        super().__init__(title, year, owner, age_restriction)
