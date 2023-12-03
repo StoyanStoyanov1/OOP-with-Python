358fe5a7f657b2c18f23d731b33d97ab4a71dacc
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-12-03 02:06:26 +0100

    Create guitarist.py

---
+++ exams/retake_exam_19_december_2022/project/band_members/guitarist.py
@@ -0,0 +1,21 @@
+from project.band_members.musician import Musician
+
+
+class Guitarist(Musician):
+    TYPE_OF_MUSICIAN = ["play metal",
+                        "play rock",
+                        "play jazz"]
+
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+
+    def learn_new_skill(self, new_skill):
+        if new_skill not in self.TYPE_OF_MUSICIAN:
+            raise ValueError(f"{new_skill} is not a needed skill!")
+
+        if new_skill in self.skills:
+            raise Exception(f"{new_skill} is already learned!")
+
+        self.skills.append(new_skill)
+        return f"{self.name} learned to {new_skill}."
+
