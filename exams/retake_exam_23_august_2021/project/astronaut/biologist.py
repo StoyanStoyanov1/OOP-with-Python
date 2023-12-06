5ea7092c4da54a91ff9eba4e61f909f110219298
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-12-07 00:22:29 +0100

    s

--- exams/retake_exam_23_august_2021/project/astronaut/biologist.py
+++
@@ -1,8 +0,0 @@
-from project.astronaut.astronaut import Astronaut
-
-
-class Biologist(Astronaut):
-    DECREASES_OXYGEN = 5
-
-    def __init__(self, name, oxygen=70):
-        super().__init__(name, oxygen)
--- exams/retake_exam_23_august_2021/project/astronaut/meteorologist.py
+++
@@ -1,8 +0,0 @@
-from project.astronaut.astronaut import Astronaut
-
-
-class Meteorologist(Astronaut):
-    DECREASES_OXYGEN = 15
-
-    def __init__(self, name, oxygen=90):
-        super().__init__(name, oxygen)
